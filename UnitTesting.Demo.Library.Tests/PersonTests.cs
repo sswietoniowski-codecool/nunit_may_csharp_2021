8faa560ed197be4c1b5d526ecfd4cfea89a9033c
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2021-12-10 10:12:39 +0100

    Basic tests implemented

---
+++ UnitTesting.Demo.Library.Tests/PersonTests.cs
@@ -0,0 +1,111 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTesting.Demo.Library.Tests
+{
+    [TestFixture]
+    public class PersonTests
+    {
+        [Test]
+        [Order(1)]
+        public void Constructor_WhenCalled_ShouldInitializeProperties()
+        {
+            // arrange
+            const string firstName = "Jan";
+            const string lastName = "Kowalski";
+            const int birthYear = 2000;
+
+            // act
+            Person person = new Person(firstName, lastName, birthYear);
+
+            // assert
+            Assert.AreEqual(firstName, person.FirstName, "checking FirstName");
+            Assert.AreEqual(lastName, person.LastName, "checking LastName");
+            Assert.AreEqual(birthYear, person.BirthYear, "checking BirthYear");
+        }
+
+        [Test]
+        [Order(2)]
+        [Ignore("Replaced by the test new test")]
+        public void FullName_WhenCalled_ShouldReturnProperName()
+        {
+            // arrange
+            const string firstName = "Jan";
+            const string lastName = "Kowalski";
+            const int birthYear = 2000;
+            string expectedFullName = $"{firstName} {lastName}";
+            Person person = new Person(firstName, lastName, birthYear);
+
+            // act
+            string actualFullName = person.FullName;
+
+            // assert
+            Assert.AreEqual(expectedFullName, actualFullName);
+        }
+
+        [Test]
+        [Order(3)]
+        [TestCase("Jan", "Kowalski")]
+        [TestCase("Anna", "Nowak")]
+        public void FullName_WhenCalled_ShouldReturnProperName(string firstName, string lastName)
+        {
+            // arrange
+            const int birthYear = 2000;
+            string expectedFullName = $"{firstName} {lastName}";
+            Person person = new Person(firstName, lastName, birthYear);
+
+            // act
+            string actualFullName = person.FullName;
+
+            // assert
+            Assert.AreEqual(expectedFullName, actualFullName);
+        }
+
+        [Test]
+        [Order(4)]
+        [TestCaseSource(nameof(Age_TestData))]
+        public void Age_WhenBirthYearBeforeOrEqualCurrentYear_ShouldCalculateProperAge(int birthYear, int currentYear, int expectedAge)
+        {
+            // arrange
+            const string firstName = "Jan";
+            const string lastName = "Kowalski";
+            Person person = new Person(firstName, lastName, birthYear);
+
+            // act
+            int actualAge = person.Age(currentYear);
+
+            // assert
+            Assert.AreEqual(expectedAge, actualAge);
+        }
+
+        [Test]
+        public void Age_WhenBirthYearAfterCurrentYear_ShouldThrowException()
+        {
+            // arrange
+            const string firstName = "Jan";
+            const string lastName = "Kowalski";
+            const int birthYear = 2000;
+            Person person = new Person(firstName, lastName, birthYear);
+            const int currentYear = 1999;
+
+            // act
+            // assert
+            Assert.Throws<ArgumentException>(() => person.Age(currentYear));
+        }
+
+        private static IEnumerable Age_TestData
+        {
+            get
+            {
+                yield return new TestCaseData(2000, 2001, 1).SetName("Age_WhenProperCurrentYear_ShouldCalculateProperAge - 1st case");
+                yield return new TestCaseData(2000, 2018, 18).SetName("Age_WhenProperCurrentYear_ShouldCalculateProperAge - 2nd case");
+                yield return new TestCaseData(2000, 2021, 21);
+            }
+        }
+    }
+}
