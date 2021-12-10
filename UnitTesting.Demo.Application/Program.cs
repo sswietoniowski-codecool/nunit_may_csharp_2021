1b953eef2fa931d0c142bb4b15f16e2f9dfc4a31
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2021-12-10 09:06:23 +0100

    Add project files.

---
+++ UnitTesting.Demo.Application/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnitTesting.Demo.Library;
+
+namespace UnitTesting.Demo.Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Jan", "Kowalski", 2000);
+            Console.WriteLine(person.FirstName);
+            Console.WriteLine(person.LastName);
+            Console.WriteLine(person.Age(2021));
+            Console.WriteLine(person.FullName);
+        }
+    }
+}
---
+++ UnitTesting.Demo.Library/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTesting.Demo.Library
+{
+    public class Person
+    {
+        public string FirstName { get; } // Jan
+        public string LastName { get; } // Kowalski
+        public int BirthYear { get; } // 2000
+
+        public Person(string firstName, string lastName, int birthYear)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            BirthYear = birthYear;
+        }
+
+        public int Age(int currentYear)
+        {
+            if (BirthYear > currentYear)
+            {
+                throw new ArgumentException();
+            }
+
+            return currentYear - BirthYear;
+        }
+
+        public string FullName => $"{FirstName} {LastName}";
+    }
+}
